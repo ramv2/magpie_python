2cc3c81931eeca4f9cd6cceddeb5d1e9abf268d1
denfed <dcfedori@buffalo.edu>
2017-11-24 16:49:07 -0500

    Added LatticeSimilarityAttributeGenerator class with successful test class

---
+++ attributes/generators/crystal/LatticeSimilarityAttributeGenerator.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import types
+from data.materials.AtomicStructureEntry import AtomicStructureEntry
+
+class LatticeSimilarityAttributeGenerator:
+    """
+    Compute similarity of structure to several simple lattices. Determined by
+    comparing the shape of each coordination polyhedron in the structure (as determined
+    using a Voronoi tessellation) to those in a reference lattice.
+
+    <p>Similarity is computed by summing the difference in the number of faces
+    with each number of edges between a certain Voronoi cell and that of the reference
+    lattice. This difference is then normalized by the number of faces in the
+    reference lattice, and averaged over all atoms to produce a "similarity index."
+    In this form, structures based on the reference lattice have a match of 0, which becomes
+    larger with increase dissimilarity.
+
+    <p>For now we consider the BCC, FCC (which has the same coordination polyhedron
+    shape as HCP), and SC lattices.
+    <usage><p><b>Usage</b>: *No options*</usage>
+
+    @author Logan Ward
+    """
+
+    def generate_features(self, entries, verbose=None):
+        # Raise exception if input argument is not of type list of
+        # AtomicStructureEntry's.
+
+        if (type(entries) is not types.ListType):
+            raise ValueError("Argument should be of type list of "
+                             "AtomicStructureEntry's")
+        elif (entries and not isinstance(entries[0], AtomicStructureEntry)):
+            raise ValueError("Argument should be of type list of "
+                             "AtomicStructureEntry's")
+
+        feat_headers = []
+        feat_values = []
+
+        feat_headers.append("dissimilarity_FCC")
+        feat_headers.append("dissimilarity_BCC")
+        feat_headers.append("dissimilarity_SC")
+
+        for entry in entries:
+            temp_list = []
+            output = entry.compute_voronoi_tessellation()
+
+            fcc = output.mean_fcc_dissimilarity()
+            bcc = output.mean_bcc_dissimilarity()
+            sc = output.mean_sc_dissimilarity()
+
+            temp_list.append(fcc)
+            temp_list.append(bcc)
+            temp_list.append(sc)
+
+            feat_values.append(temp_list)
+
+        features = pd.DataFrame(feat_values, columns=feat_headers)
+
+        if verbose:
+            print features.head()
+
+        return features
+
+
